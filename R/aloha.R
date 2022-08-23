70e2c206558d2216203b31a70669d82c7358cbc2
Atahualpa_Ayala <ayala@gmail.com>
2022-08-23 22:39:53 +0000

    try this

--- R/aloha.R
+++ R/aloha.R
@@ -8,16 +8,19 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-say_aloha <- function(name) {
+say_aloha <- function(name, color) {
 
   message <- paste("Aloha,",
                    name,
                    emo::ji("palm_tree"),
                    emo::ji("sunny"),
                    emo::ji("ocean"))
-
-
-  cat(crayon::cyan(message))
-
+  if (color == "blue"){
+    cat(crayon::blue(message))}
+  else if (color == "red"){
+    cat(crayon::red(message))}
+  else {
+    cat(crayon::cyan(message))
+  }
 }
 
